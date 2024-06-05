aeb1308084201753ad979df73d0216f2428be990
full name <148303817+SilyaSU@users.noreply.github.com>
2024-06-05 11:56:49 +0700

    Update MainWindow.xaml.cs

--- StroyCompany/MainWindow.xaml.cs
+++ StroyCompany/MainWindow.xaml.cs
@@ -16,20 +16,27 @@ namespace StroyCompany
             LoadData();
         }
 
-        private void LoadData()
-        {
-            string selectQuery = @"
-            SELECT Объект.*, Рабочая_группа.Название AS Рабочая_группа 
-            FROM Объект 
-            JOIN Рабочая_группа ON Объект.FK_Рабочая_группа = Рабочая_группа.id_Рабочая_группа";
-
-            database.openConnection();
-            SqlDataAdapter dataAdapter = new SqlDataAdapter(selectQuery, database.sqlConnection);
-            DataTable dataTable = new DataTable();
-            dataAdapter.Fill(dataTable);
-            objectsDataGrid.ItemsSource = dataTable.DefaultView;
-            database.closeConnection();
-        }
+         private void LoadData()
+ {
+     using (var db = new DataBase())
+     {
+         db.openConnection();
+         string query = @"
+         SELECT 
+             Объект.Название AS Название,
+             Объект.Адрес AS Адрес,
+             Объект.Тип AS Тип,
+             Рабочая_группа.Название AS Рабочая_группа,
+             Объект.Стоимость AS Стоимость
+         FROM Объект
+         LEFT JOIN Рабочая_группа ON Объект.FK_Рабочая_группа = Рабочая_группа.id_Рабочая_группа";
+         var reader = db.ExecuteQuery(query);
+         DataTable dataTable = new DataTable();
+         dataTable.Load(reader);
+         objectsDataGrid.ItemsSource = dataTable.DefaultView;
+         db.closeConnection();
+     }
+ }
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
